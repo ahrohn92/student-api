05dcb446992d0537a92e745bb9e51f8624da2969
ahrohn92 <ahrohn92@gmail.com>
2020-11-10 19:02:47 +0100

    finished

--- app.js
+++ app.js
@@ -1,38 +1,57 @@
 const express = require('express')
 const app = express()
-const users = require('./users.json')
+const students = require('./students.json')
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/users', (req, res) => res.send(users))
-
-app.get('/users/:id', (req, res) => {
-    /* GET a user by their id */
-    res.send(users[req.params.id-1])
-})
-
-app.post('/', (req, res) => {
-    /* POST user data using the request body */
-    let {name, profilePic, lastCalled, timesCalled, notes} = req.body
-    let result;
-    if(name) {
-        let newUserId = 1000
-        users.push(req.body)
-        result = {status: "Success", message: "The user was successfully added"}
-    } else {
-        result = {status: "Failed", message: "The user was not added"}
-        res.status(400)
-    }
-    res.json(result)
+app.get('/students', (req, res) =>  {
+    res.send(students)
 })
 
 app.get('/search', (req, res) => {
-    /* GET a user by their name */
-    let name = req.query.name
-    let user = users.find(user => user.name[0] === name)
-    res.send(user)
+    if (req.query) {
+        let name = req.query.name
+        res.send(students.find(student => name === student.name[0]))
+    } else {
+        res.send(students)
+    }
+})
+
+app.get('/students/:studentId', (req, res) => {
+    let id = parseInt(req.params.studentId)
+    let student = students.find(student => student.id === id)
+    res.send(student)
+})
+
+app.get('/grades/:studentId', (req, res) => {
+    let id = parseInt(req.params.studentId)
+    let student = students.find(student => student.id === id)
+    let grades = student.grades
+    res.send(grades)
+})
+
+app.post('/grades', (req, res) => {
+    let {id, name, grade} = req.body
+    let result;
+    if (id && grade) {
+        result = {status: "Success", message: "The grade was successfully recorded"}
+    } else {
+        result = {status: "Failed", message: "The grade was not recorded"}
+    }
+    res.send(result)
+})
+
+app.post('/register', (req, res) => {
+    let {username, email} = req.body
+    let result;
+    if (username && email) {
+        result = {status: "Success", message: "The user was successfully registered"}
+    } else {
+        result = {status: "Failed", message: "The user was not registered"}
+    }
+    res.send(result)
 })
 
 const port = 3000
